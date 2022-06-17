d63bc11443da73f3fdc1f6c212c7dbc33632aa49
Grazziano Fagundes <grazzianofagundes@gmail.com>
2022-06-17 12:11:26 -0300

    Aplicando grupo de estilos

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 
 export default function App() {
+  const [name, setName] = useState('Fulano');
+
+  function handlemudaNome() {
+    // alert('TESTE');
+    setName('Grazziano');
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>{name}</Text>
+      <TouchableOpacity style={styles.button} onPress={handlemudaNome}>
+        <Text style={styles.buttonText}>Mudar nome</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -13,8 +22,24 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: '#F1F1F1',
+    paddingTop: 28,
+  },
+  title: {
+    fontSize: 32,
+    color: '#121212',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  button: {
+    backgroundColor: 'blue',
+    height: 40,
     justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 35,
+  },
+  buttonText: {
+    color: '#FFF',
+    fontWeight: 'bold',
   },
 });
